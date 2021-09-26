8bf11254696412398b80a272d39c43dce409e9d6
Dave Spina <dspina79@gmail.com>
2021-09-25 20:55:51 -0400

    Data types

---
+++ js/basics/datatypes.js
@@ -0,0 +1,8 @@
+// strings
+var firstName = "Dean";
+// numbers
+var age = 40;
+// any
+var id;
+// arrays
+var myFriends = ["Brian", "Doug", "Sheryl"];
---
+++ ts/basics/datatypes.ts
@@ -0,0 +1,11 @@
+// strings
+let firstName: string = "Dean";
+
+// numbers
+let age: number = 40;
+
+// any
+let id: any;
+
+// arrays
+let myFriends: string[] = ["Brian", "Doug", "Sheryl"];
